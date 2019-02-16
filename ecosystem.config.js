3a4721a2c53a7f149e974afb0f143a82fea96b00
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-16 23:59:26 +0800

    Adjusted ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Faker REST API',
+      name: 'Faker GraphQL API',
       script: 'index.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
