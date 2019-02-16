aa9e89b1d741e8a7a009fc55de93850cc17cc4ad
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-16 13:05:11 +0800

    Added password service

---
+++ src/utils/password/index.ts
@@ -0,0 +1,25 @@
+import * as bcrypt from 'bcrypt';
+
+const ROUNDS = 14;
+
+const verify = async (
+  input: string,
+  hash: string,
+  salt: string
+): Promise<boolean> => {
+  return await bcrypt.compare(input + salt, hash);
+};
+
+const generateSalt = async (): Promise<string> => {
+  return await bcrypt.genSalt(ROUNDS);
+};
+
+const generateHash = async (input: string, salt: string): Promise<string> => {
+  return await bcrypt.hash(input + salt, ROUNDS);
+};
+
+export const passwordService = {
+  verify,
+  generateSalt,
+  generateHash
+};
