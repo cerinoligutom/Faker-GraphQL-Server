73b12f80277ebbc6a2cec8e9d4fd10c785604666
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-17 23:34:52 +0800

    Landing Page

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import compression from 'compression';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import { scheduleJob } from 'node-schedule';
+import path from 'path';
 
 import { initApolloGraphqlServer } from './graphql';
 import { maintenanceRouter } from './routes/maintenance.routes';
@@ -19,6 +20,22 @@ const startApp = async () => {
   app.use(express.json());
   app.use(compression());
   app.use(passport.initialize());
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  // set path for static assets
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.get('/', (req, res) => {
+    const variables = {
+      githubRepositoryUrl: 'https://github.com/cerino-ligutom/Faker-GraphQL-Server',
+      zeferinixSiteUrl: 'https://www.zeferinix.com',
+      graphqlEndpoint: `http://faker-graphql.zeferinix.com/graphql`
+    }
+    res.render('index', variables);
+  });
 
   app.use('/api/maintenance', maintenanceRouter);
 
@@ -43,6 +60,10 @@ const startApp = async () => {
         data: env.isProduction ? {} : err
       }
     });
+  });
+
+  app.get('*', (req, res) => {
+    res.redirect('/');
   });
 
   app.listen(env.PORT, () => {
