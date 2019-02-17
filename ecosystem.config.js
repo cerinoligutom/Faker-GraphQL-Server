9aff7326b0610a76e7f5cb09b68cdb5724388c5e
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-18 00:38:38 +0800

    Increased max restarts to 5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       autorestart: true,
-      max_restarts: 1,
+      max_restarts: 5,
       min_uptime: '10s',
       watch: ['src/**/*.ts'],
       max_memory_restart: '1G'
