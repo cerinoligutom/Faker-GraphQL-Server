d2ba564f74ac576b70c27e021e96ebc5771d7cfd
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-18 00:33:29 +0800

    Refactoring

--- src/graphql-shield/rules/isAuthenticated.ts
+++ src/graphql-shield/rules/isAuthenticated.ts
@@ -1,11 +1,14 @@
-import { rule } from 'graphql-shield';
 import { IGraphQLContext } from '@app/utils';
 import { AuthenticationError } from 'apollo-server-express';
+import { rule } from 'graphql-shield';
 
-export const isAuthenticated = rule()(async (parent, args, ctx: IGraphQLContext, info) => {
-  if (ctx.user) {
-    return true;
-  } else {
-    return new AuthenticationError('You are not authenticated. Try logging in.');
+export const isAuthenticated = rule()(
+  async (parent, args, ctx: IGraphQLContext, info) => {
+    if (ctx.user) {
+      return true;
+    }
+    return new AuthenticationError(
+      'You are not authenticated. Try logging in.'
+    );
   }
-});
+);
